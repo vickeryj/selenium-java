a4a367b55633f9987c877919204f28a9926ef2c4
Joshua Vickery <josh@thesquarefoot.com>
2018-02-20 17:42:51 -0500

    add missing imports

--- src/test/java/com/squarefoot/SignupTest.java
+++ src/test/java/com/squarefoot/SignupTest.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SignupTest {
 
