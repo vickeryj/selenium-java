b099f7ae0a0771af5e3cc429a8b939ee33c4be08
Joshua Vickery <josh@thesquarefoot.com>
2018-02-27 11:05:38 -0500

    rina code

--- src/test/java/com/squarefoot/SampleTest.java
+++ src/test/java/com/squarefoot/SampleTest.java
@@ -3,6 +3,7 @@ package com.squarefoot;
 import java.net.URL;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -12,7 +13,14 @@ public class SampleTest {
   @Test
   public void testGoogle() throws java.net.MalformedURLException {
     WebDriver driver  = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.chrome());
-    driver.navigate().to("http://google.com");
-    driver.quit();
+    driver.navigate().to("http://www.squarefoot.com");
+    driver.findElement(By.className("button")).click();
+    for(int i = 0 ; i < 4 ; i++) {
+      driver.findElement(By.xpath("//*[@id='app']/div/main/div/main/footer/button")).click();
+    }
+    driver.findElement(By.name("email")).sendKeys("josh@squarefoot.com");
+    driver.findElement(By.xpath("//*[@id='app']/div/main/div/main/form/div/input[3]")).click();
+    
+    // driver.quit();
   }
 }
